371c81905b7c8907e8ae0ef6e3319b9825305fed
Richard Tjerngren <rtjerngren@spotify.com>
2019-11-14 15:43:59 +0100

    wip

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	let counter = 0;
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello, counter is {counter}!</h1>
+<button on:click="{() => counter += 1}">+</button>
+<button on:click="{() => counter -= 1}">-</button>
