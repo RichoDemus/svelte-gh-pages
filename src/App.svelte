8185c0b25aee8e9fab7d90d1cd18cfe8a473605f
Richard Tjerngren <rtjerngren@spotify.com>
2019-11-15 16:16:36 +0100

    test key presses

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,27 @@
 	const unsubscribe = count.subscribe(value => {
 		counter = value;
 	});
+
+	let key;
+	let keyCode;
+	function handleKeydown(event) {
+		key = event.key;
+		keyCode = event.keyCode;
+
+		if(key === "+") {
+			increment();
+		} else if (key === "-") {
+			decrement();
+		}
+	}
+
+	function increment() {
+		count.update(n => n + 1)
+	}
+
+	function decrement() {
+		count.update(n => n - 1)
+	}
 </script>
 
 <style>
@@ -13,6 +34,9 @@
 	}
 </style>
 
+<svelte:window on:keydown={handleKeydown}/>
+
 <h1>Hello, counter is {counter}!</h1>
-<button on:click="{() => count.update(n => n + 1)}">+</button>
-<button on:click="{() => count.update(n => n - 1)}">-</button>
+<button on:click="{() => increment()}">+</button>
+<button on:click="{() => decrement()}">-</button>
+<p>key: {key}, key code: {keyCode}</p>
